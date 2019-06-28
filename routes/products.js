0a12769c2b17f219b6d63dd3e1827925a3c03c73
elenore29 <elenore.nava@gmail.com>
2019-06-28 11:04:55 -0500

    Implementando funciones de put y delete para products

--- routes/products.js
+++ routes/products.js
@@ -1,39 +1,67 @@
-//2. Middlewares / Cadeneros
-//Modelos 
-const {Products} = require('../models/Products');
+const {
+    requireAdmin
+} = require('../middleware/auth');
+
+const {
+    Products
+} = require('../models/Products');
 
 module.exports = (app, next) => {
-app.get('/products', (req, res) => {
-    console.log(req.body)
-
-    Products.find({}, (err, products) => {
-         if (err) {
-            return err
-        }
-        res.send(products);
+    app.get('/products', (req, res) => {
+        Products.find({}, (err, products) => {
+            if (err) {
+                return err
+            }; 
+            res.send(products);
+        });
     });
-});
 
-app.post('/products', (req, res) => {
-    console.log(req.body)
-    const newProduct = new Products(req.body)
-    console.log(newProduct)
-    newProduct.save((err, data) => {
-        if (err) {
-            return err
-        }
-        res.send(data)
-    }); 
-}); 
+    app.post('/products', requireAdmin, (req, res) => {
+        const newProduct = new Products(req.body)
+        newProduct.save((err, data) => {
+            if (err) {
+                return err
+            }
+            res.send(data)
+        });
+    });
 
-app.get('/products/:id', (req, res) => {
-    Products.find({_id: req.params.id},(err, data) => {
-        if (err) {
-            return err
-        }
-        res.send(data)
-    }); 
-}); 
+    app.get('/products/:id', (req, res) => {
+        Products.find({
+            _id: req.params.id
+        }, (err, data) => {
+            if (err) {
+                return err
+            }
+            res.send(data);
+        });
+    });
 
-return next(); 
-}
+    app.put('/products/:id', requireAdmin, (req, res) => {
+        Products.findOneAndUpdate({
+                _id: req.params.id
+            }, req.body, {
+                new: true
+            })
+            .then(function () {
+                Products.findOne({
+                    _id: req.params.id
+                }).then(function (data) {
+                    res.send(data);
+                });
+            });
+    });
+
+    app.delete('/products/:id', requireAdmin, (req, res, next) => {
+        Products.deleteOne({
+                _id: req.params.id
+            })
+            .then(() => {
+                return Products.find();
+            })
+            .then(doc => res.json(doc))
+            .catch(next);
+    });
+
+    return next();
+};
